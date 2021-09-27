c2e59e2fef73a9781965e0456ce4ae3e53c411d9
Rajiv Seelam <rajiv@betalectic.com>
2021-09-27 22:13:59 +0530

    Add update script

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,7 @@ module.exports = async () => {
   return {
     bail: true,
     verbose: true,
-    testMatch: [
-      "**/__tests__/**/*.[jt]s?(x)",
-      "**/?(*.)+(spec|test).[jt]s?(x)",
-    ],
+    testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
     setupFiles: ["./tests/jest-setup.js"],
     setupFilesAfterEnv: ["./tests/jest-after.js"],
     globalSetup: "./tests/global-setup.js",
---
+++ update.js
@@ -0,0 +1,143 @@
+const fs = require("fs-extra");
+const url = require("url");
+const path = require("path");
+const https = require("https");
+var extractZip = require("extract-zip");
+
+const releaseOptions = {
+  hostname: "api.github.com",
+  port: 443,
+  path: "/repos/nobejs/nobejs/releases/latest",
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "User-Agent": "request",
+  },
+};
+
+const httpsRequestPromise = (options) => {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.setEncoding("utf8");
+      let responseBody = "";
+
+      res.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+
+      res.on("end", () => {
+        resolve(JSON.parse(responseBody));
+      });
+    });
+
+    req.on("error", (err) => {
+      reject(err);
+    });
+
+    req.end();
+  });
+};
+
+const updateRelease = (options) => {
+  fs.rmdirSync("upgrade-nobejs", { recursive: true });
+
+  if (!fs.existsSync("upgrade-nobejs")) {
+    fs.mkdirSync("upgrade-nobejs");
+  }
+
+  return new Promise((resolve, reject) => {
+    https
+      .get(options, function (res1) {
+        console.log("Release Asset:", res1.headers.location);
+
+        // res1.pipe(dest);
+
+        https
+          .get(res1.headers.location, function (res2) {
+            console.log(
+              "res2.header"
+              //   res2.headers,
+            );
+            var filename = res2.headers["content-disposition"].replace(
+              "attachment; filename=",
+              ""
+            );
+
+            var destf = path.join("upgrade-nobejs", filename);
+            var dest = fs.createWriteStream(destf);
+
+            var total = parseInt(res2.headers["content-length"]);
+            // console.log("total", total);
+            var completed = 0;
+            res2.pipe(dest);
+            res2.on("data", function (data) {
+              completed += data.length;
+              //   console.log("completed", (completed / total) * 100);
+            });
+            res2.on("error", () => {
+              console.log(error.message);
+            });
+            res2.on("end", async () => {
+              console.log("Download to:", destf);
+              try {
+                const extractionResult = await extractZip(destf, {
+                  dir: path.resolve("upgrade-nobejs/release"),
+                });
+                let releaseFiles = fs.readdirSync(
+                  path.resolve("upgrade-nobejs/release")
+                );
+                console.log(
+                  "Extracted release.zip into folder release: with name:",
+                  releaseFiles[0]
+                );
+
+                console.log(
+                  "Copy What: ",
+                  path.resolve(`upgrade-nobejs/release/${releaseFiles[0]}/core`)
+                );
+
+                console.log("Copy: To", path.resolve(`core`));
+
+                fs.moveSync(path.resolve(`core`), path.resolve(`core_bk`));
+                fs.moveSync(
+                  path.resolve(
+                    `upgrade-nobejs/release/${releaseFiles[0]}/core`
+                  ),
+                  path.resolve(`core`)
+                );
+                fs.rmdirSync("upgrade-nobejs", { recursive: true });
+                fs.rmdirSync("core_bk", { recursive: true });
+              } catch (err) {
+                fs.rmdirSync("core", { recursive: true });
+                fs.moveSync(path.resolve(`core_bk`), path.resolve(`core`));
+
+                console.log("err", err);
+              }
+            });
+          })
+          .on("error", () => {});
+      })
+      .on("error", (error) => {
+        console.log("Go crazy", error);
+      });
+  });
+};
+
+fs.rmdirSync("upgrade", { recursive: true });
+
+(async () => {
+  try {
+    let response = await httpsRequestPromise(releaseOptions);
+    console.log("Latest Release:", response.zipball_url);
+
+    var zipBallOptions = Object.assign({}, url.parse(response.zipball_url), {
+      headers: {
+        "User-Agent": "nobejs",
+      },
+    });
+
+    await updateRelease(zipBallOptions);
+  } catch (error) {
+    console.log("error", error);
+  }
+})();
