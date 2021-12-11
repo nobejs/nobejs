54223e417e467d03ac588c5759b7686f0739de78
Rajiv Seelam <rajiv@betalectic.com>
2021-12-12 01:24:48 +0530

    Add baseRepo file

---
+++ core/utils/baseRepo.js
@@ -0,0 +1,98 @@
+const knex = requireKnex();
+
+const addCreatedTimestamps = (payload) => {
+  payload["created_at"] = new Date().toISOString();
+  payload["updated_at"] = new Date().toISOString();
+  return payload;
+};
+
+const create = async (table, payload) => {
+  try {
+    payload = addCreatedTimestamps(payload);
+    let result = await knex.transaction(async (trx) => {
+      const rows = await trx(table).insert(payload).returning("*");
+      return rows[0];
+    });
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const update = async (table, where, payload) => {
+  try {
+    payload["updated_at"] = new Date().toISOString();
+    let rows = await knex(table).where(where).update(payload).returning("*");
+    return rows[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+const remove = async (table, where, mode = "soft") => {
+  try {
+    if (mode === "soft") {
+      let row = await knex(table).where(where).update({
+        deleted_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      });
+
+      return row;
+    }
+
+    if (mode === "hard") {
+      let rows = await knex(table)
+        .where(where)
+        .whereNull("deleted_at")
+        .delete();
+      return rows;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const first = async (table, where = {}) => {
+  try {
+    let row = await knex(table).where(where).whereNull("deleted_at").first();
+    return row;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const countAll = async (table, where = {}, whereNot = {}) => {
+  try {
+    let rows = await knex(table)
+      .where(where)
+      .whereNot(whereNot)
+      .whereNull("deleted_at")
+      .count({ count: "*" })
+      .first();
+    return parseInt(rows.count);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findAll = async (table, where = {}, columns) => {
+  try {
+    let rows = await knex(table)
+      .where(where)
+      .whereNull("deleted_at")
+      .select(columns);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  addCreatedTimestamps,
+  create,
+  remove,
+  update,
+  first,
+  findAll,
+  countAll,
+};
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const Config = require("./config")();
 const httpServer = requireHttpServer();
 
-const server = httpServer({
-  logger: process.env.DEBUG === "true" ? true : false,
-});
+const server = httpServer({});
 
 server.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
   if (err) {
