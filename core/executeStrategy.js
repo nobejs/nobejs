7606af7cf81e73a979b1e5cf659be4713d53fbf9
Shubham Joshi <shubham@betalectic.com>
2021-12-17 18:47:13 +0530

    replaced sequence with handlerFunctions while looping in executeStrategy

--- core/executeStrategy.js
+++ core/executeStrategy.js
@@ -22,7 +22,7 @@ module.exports = (sequence) => {
       );
 
       try {
-        for (let ord of sequence) {
+        for (let ord of handlerFunctions) {
           if (ord.charAt(0) === "*") {
             ord = ord.substring(1);
             if (!context[ord]) {
