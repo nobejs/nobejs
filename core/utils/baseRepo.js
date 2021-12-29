e3361a0a3826894a562951a7dc8fbd82c4ef45ac
Rajiv Seelam <rajiv@betalectic.com>
2021-12-29 17:01:04 +0530

    Introduce throwNotFound

--- core/utils/baseRepo.js
+++ core/utils/baseRepo.js
@@ -56,15 +56,18 @@ const remove = async (table, where, mode = "soft") => {
   }
 };
 
-const first = async (table, where = {}) => {
+const first = async (table, where = {}, throwNotFound = false) => {
   try {
     let row = await knex(table).where(where).whereNull("deleted_at").first();
 
-    if (row === undefined) {
-      throw {
-        statusCode: 404,
-        message: "Not Found",
-      };
+    if (throwNotFound) {
+      if (row === undefined) {
+        throw {
+          errorCode: "NotFound",
+          statusCode: 404,
+          message: "Not Found",
+        };
+      }
     }
 
     return row;
