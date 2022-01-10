5e2c6577bcfdadb68242c60e2378a1e48a664101
Rajiv Seelam <rajiv@betalectic.com>
2022-01-10 12:07:56 +0530

    Add sentry

--- core/errorHandler.js
+++ core/errorHandler.js
@@ -1,3 +1,13 @@
+const Sentry = require("@sentry/node");
+
+if (process.env.SENTRY_DSN !== "") {
+  Sentry.init({
+    environment: process.env.SENTRY_ENVIRONMENT,
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  });
+}
+
 module.exports = (err, req, res) => {
   if (err.statusCode) {
     let statusCode = err.statusCode;
@@ -8,6 +18,9 @@ module.exports = (err, req, res) => {
   if (process.env.DEBUG === "true") {
     return res.status(500).send({ message: err.message });
   } else {
+    if (process.env.SENTRY_DSN !== "") {
+      Sentry.captureException(err);
+    }
     return res.status(500).send({ message: "Something went wrong" });
   }
 };
