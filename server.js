c394fbd39f547531bdbb87da97eca0d72cfddd05
Rajiv Seelam <rajiv@betalectic.com>
2022-06-24 18:34:05 +0530

    Upgrade fastify and nodejs default version

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const httpServer = requireHttpServer();
 
 const server = httpServer({});
 
-server.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+server.listen(
+  { port: process.env.PORT || 3000, host: "0.0.0.0" },
+  (err, address) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
   }
-});
+);
