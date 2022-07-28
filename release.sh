95ca61d4fc792857d5f0872a45c2333d99a5826e
Rajiv Seelam <rajiv@betalectic.com>
2022-07-28 15:46:24 +0530

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+eval yarn bump:patch
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+echo $PACKAGE_VERSION
+
+cp="gh release create v$PACKAGE_VERSION --notes \"v$PACKAGE_VERSION\" -p"
+
+echo $cp
+
+eval "$cp"
