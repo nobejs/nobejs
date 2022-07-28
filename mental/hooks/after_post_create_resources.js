1aff2c784142febd9a7dd47ba386b59ceab81191
Rajiv Seelam <rajiv@betalectic.com>
2022-07-28 14:47:53 +0530

    Delete hooks

--- mental/hooks/after_post_create_resources.js
+++
@@ -1,3 +0,0 @@
-module.exports = async () => {
-  console.log("after_post_create_resources");
-};
--- mental/hooks/before_post_create_resource.js
+++
@@ -1,5 +0,0 @@
-const findKeysFromRequest = requireUtil("findKeysFromRequest");
-
-module.exports = async (req, payload) => {
-  return { team_uuid: "71151aad-e6a8-4dee-9f46-ddac466cb409" };
-};
