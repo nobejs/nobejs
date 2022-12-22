308ff51555d03daf603c05815b60e1dd1a0a28e9
Rajiv Seelam <rajiv@betalectic.com>
2022-12-22 12:39:13 +0530

    Remove mental refs

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,5 @@
 module.exports = (app) => {
   app.get("/liveness", async (req, res) => {
-    // mentalEngine.run();
-    // mentalEngine.execute("createPosts");
     return res.code(200).send({ status: "I am alive" });
   });
 
